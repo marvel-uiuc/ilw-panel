e5716be922329c033399ca982082d78c83e11d60
Mikael Arvola <marvel@illinois.edu>
2024-11-21 10:11:30 -0600

    Initial implementation.

--- src/ilw-panel.ts
+++ src/ilw-panel.ts
@@ -2,13 +2,20 @@ import { LitElement, html, unsafeCSS } from "lit";
 // @ts-ignore
 import styles from './ilw-panel.styles.css?inline';
 import './ilw-panel.css';
-import { customElement, property } from "lit/decorators.js";
+import { customElement, property, queryAssignedNodes } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
 
 @customElement("ilw-panel")
 export default class Panel extends LitElement {
 
     @property()
     theme = "";
+
+    @property({state: true, type: Boolean})
+    hasFooter = false;
+
+    @queryAssignedNodes({slot: 'footer'})
+    footerNodes!: Array<Node>;
 
     static get styles() {
         return unsafeCSS(styles);
@@ -18,11 +25,32 @@ export default class Panel extends LitElement {
         super();
     }
 
+    /**
+     * Tracks the number of graphic elements (images and icons) in the card, so we can
+     * hide the graphics container if there's no graphics.
+     */
+    private slotsChanged() {
+        this.hasFooter = this.footerNodes?.length > 0;
+    }
+
     render() {
+
+        let footerClasses = {
+            "footer": true,
+            "has-content": this.hasFooter
+        }
+
         return html`
-            <div>
+            <article class="panel-inner">
+                <slot name="heading"></slot>
                 <slot></slot>
-            </div>
+                <div class=${classMap(footerClasses)}>
+                    <div class="border"></div>
+                    <div class="footer-inner">
+                        <slot name="footer" @slotchange=${this.slotsChanged}></slot>
+                    </div>
+                </div>
+            </article>
         `;
     }
 }
